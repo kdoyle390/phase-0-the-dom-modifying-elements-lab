3ca075bdea27cc784dd610bcf338db9902b841f0
Kelsey <kdoyle390@gmail.com>
2021-09-09 10:45:25 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+//remove main
+main.remove();
+
+//'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Kelsey is the champion"
+
+
+
+
